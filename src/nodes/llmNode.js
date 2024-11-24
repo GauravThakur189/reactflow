654b983d525dc570c8c0de91ec1269184da90b88
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-11-24 22:51:03 +0530

    spaxe

--- src/nodes/llmNode.js
+++ src/nodes/llmNode.js
@@ -1,29 +1,5 @@
-// import React from 'react';
-// import BaseNode from './baseNodee';
-// import { Position } from 'reactflow';
-// export const LLMNode = ({ id, data }) => {
-//   return (
-//     <BaseNode
-//       id={id}
-//       type="LLM"
-//       data={data}
-//       handles={[
-//         { type: 'target', position: Position.Left, id: `${id}-system`, style: { top: `${100 / 3}%` } },
-//         { type: 'target', position: Position.Left, id: `${id}-prompt`, style: { top: `${200 / 3}%` } },
-//         { type: 'source', position: Position.Right, id: `${id}-response` },
-//       ]}
-//       content={() => <span>Hello! This is an LLM.</span>}
-//     />
-//   );
-// };
-
-
-
-
-
-
 import React from "react";
-import BaseNode from './baseNodee';// Import the updated BaseNode
+import BaseNode from './baseNodee';
 import { Position } from "reactflow";
 
 export const LLMNode = ({ id, data }) => {
